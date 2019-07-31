737b3150a036706197cdcaf97583f0b34e9b0f9a
Akash <HacK3r@MSI.local>
2019-08-01 01:48:52 +0530

    try to use function

--- Remote_cmd.py
+++ Remote_cmd.py
@@ -2,29 +2,33 @@
 
 import sys, paramiko, csv
 command = sys.argv[1]
-username = "abc"
+username = "root"
 port = 22
-password = "xyz"
+password = "toor"
 X = {}
 w = csv.writer(open("output.csv", "w"))
 f = open("server.txt","r")
 Inp=[line.rstrip('\n') for line in f]
-for server in Inp:
+def getouput(host):
     try:
-        print(server)
+        print(host)
         client = paramiko.SSHClient()
         client.load_system_host_keys()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(server, port=port, username=username, password=password, pkey=None, key_filename=None)
+        client.connect(host, port=port, username=username, password=password, pkey=None, key_filename=None)
         stdin, stdout, stderr = client.exec_command(command)
         a = stdout.read()
-        X.update({server: a})
+        X.update({host: a})
+        return host,a
     except:
         a = sys.exc_info()[0]
-        X.update({server: a})
+        X.update({host: a})
+        return host,a
     finally:
         client.close()
+
+for server in Inp:
+    print (getouput(server))
 for key, val in X.items():
     w.writerow([key, val])
-    print("Output.csv has been exported at script root path")
-
+print("Output.csv has been exported at script root path")
